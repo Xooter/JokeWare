0f67ee4f159574e6ce8f925d3666482daedb2bc6
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-29 19:09:46 -0300

    all command function

--- server/src/Commands.cpp
+++ server/src/Commands.cpp
@@ -4,6 +4,13 @@
 CommandType Commands::getCommandType(const string &command) {
   if (command == "mouse")
     return MOUSE;
+  else if (command == "wallpaper")
+    return WALLPAPER;
+  else if (command == "os")
+    return OS;
+  else if (command == "dialog")
+    return DIALOG;
+
   return UNKNOWN;
 }
 
@@ -15,10 +22,40 @@ void Commands::processCommand(const char *command, const char *params) {
 void Commands::acceptCommand(CommandType command, const string &params) {
   switch (command) {
   case MOUSE:
+    mouseCommand(params);
+    break;
+  case WALLPAPER:
+    wallpaperCommand(params);
+    break;
+  case OS:
+    osCommand(params);
+    break;
+  case DIALOG:
+    dialogCommand(params);
     break;
   default:
     string response = "Comando desconocido";
     send(clientSocket, response.c_str(), response.length(), 0);
     break;
   }
+}
+
+void Commands::mouseCommand(const string &params) {
+  string response = "Comando mouse";
+  send(clientSocket, response.c_str(), response.length(), 0);
+}
+
+void Commands::wallpaperCommand(const string &params) {
+  string response = "Comando wallpaper";
+  send(clientSocket, response.c_str(), response.length(), 0);
+}
+
+void Commands::osCommand(const string &params) {
+  string response = "Comando os";
+  send(clientSocket, response.c_str(), response.length(), 0);
+}
+
+void Commands::dialogCommand(const string &params) {
+  string response = "Comando dialog";
+  send(this->clientSocket, response.c_str(), response.length(), 0);
 }
--- server/src/Commands.hpp
+++ server/src/Commands.hpp
@@ -4,15 +4,18 @@
 
 using namespace std;
 
-enum CommandType { MOUSE, UNKNOWN };
+enum CommandType { MOUSE, WALLPAPER, OS, DIALOG, UNKNOWN };
 class Commands {
 private:
   CommandType getCommandType(const string &command);
   void acceptCommand(CommandType command, const string &params);
 
+  void mouseCommand(const string &params);
+  void wallpaperCommand(const string &params);
+  void osCommand(const string &params);
+  void dialogCommand(const string &params);
+
 public:
   int clientSocket;
   void processCommand(const char *command, const char *params);
 };
-
-void acceptCommand(const char *command, const char *params);
