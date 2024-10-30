b9ed1bde1ccfced0b784c6755dc81f7111f1a7ec
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-30 10:26:01 -0300

    client params

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
@@ -8,6 +9,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    cerr << "Uso: " << argv[0] << " <comando> <mensaje>" << endl;
+    return 1;
+  }
+
+  const char *ipAddress = "127.0.0.1";
+  const char *command = argv[1];
+  const char *message = argv[2];
+
   int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
   sockaddr_in serverAddress;
@@ -15,20 +25,25 @@ int main(int argc, char *argv[]) {
   serverAddress.sin_port = htons(PORT);
   serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+  if (inet_pton(AF_INET, ipAddress, &serverAddress.sin_addr) <= 0) {
+    cerr << "Dirección IP no válida: " << ipAddress << endl;
+    return 1;
+  }
+
   int _ = connect(clientSocket, (struct sockaddr *)&serverAddress,
                   sizeof(serverAddress));
 
-  const char *message = argv[1];
+  string fullMessage = string(command) + "@" + message;
 
-  send(clientSocket, message, strlen(message), 0);
+  send(clientSocket, fullMessage.c_str(), strlen(fullMessage.c_str()), 0);
 
-  cout << "Mensaje enviado al servidor: " << message << endl;
+  cout << "Mensaje: " << fullMessage << endl;
 
   // recv
   char buffer[1024] = {0};
   int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
   if (bytesReceived > 0) {
-    cout << "Respuesta del servidor: " << string(buffer, bytesReceived) << endl;
+    cout << "Respuesta: " << string(buffer, bytesReceived) << endl;
   } else {
     cerr << "Error al recibir la respuesta del servidor." << endl;
   }
